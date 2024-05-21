54ac38d787d846db83701b4496583e31ee59a510
Junyeok Seo <jun2342@chungbuk.ac.kr>
2024-05-21 21:40:45 +0900

    second commit

--- client.cpp
+++ client.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "client.h"
+#include "libxl.h"
 
 Client::Client()
 {
-	this->id = 1;
+	this->id = "1";
 	this->name = "È«±æµ¿";
 	this->gender = "³²";
 	this->age = 25;
 	this->phonenumber = 01011112222;
 	this->visit = 1;
+}
+
+void Client::writeExcel(std::string& filename, Client* list, int size)
+{
+	libxl::Book* book = xlCreateBook();
+	if (book)
+	{
+		libxl::Sheet* sheet = book->addSheet(L"Sheet1");
+		if (sheet)
+		{
+			sheet->writeStr(2, 1, L"Hello, World !");
+			sheet->writeNum(3, 1, 1000);
+		}
+		book->save(L"file.xls");
+		book->release();
+	}
 }
--- client.h
+++ client.h
@@ -8,13 +8,14 @@ class Client
 private:
 	std::string name;
 	std::string gender;
-	int id;
+	std::string id;
 	int age;
 	int phonenumber;
 	int visit;
 
 public:
 	Client();
+	void writeExcel(std::string& filename, Client* list, int size);
 };
 
 #endif
